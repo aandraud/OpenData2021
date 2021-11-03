039c7060c1adeb6105dc5eac88bba2b939369a9a
NFL <lulu57580@gmail.com>
2021-11-03 09:49:01 +0100

    Ajout fonction

--- public/javascript/fonction_share.js
+++ public/javascript/fonction_share.js
@@ -1,3 +1,14 @@
+const http = require('https')
+
 exports.get_dep_info = (res, req) => {
     console.log('fonction_get_info');
+};
+
+exports.get_open_info() => {
+    url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=covid-19-france-vaccinations-age-sexe-dep&q=&sort=date&facet=date&facet=variable&facet=variable_label&facet=dep_name&facet=reg_code&facet=reg_name&facet=dep_area_code"
+    http.get(url,(resp)=>{
+        var data = resp.body
+        data = JSON.parse(data)
+        resp.status(200).send(data)
+    })
 }
