9f46c295424c81f5150932583ecf1178c07d8b3e
NFL <lulu57580@gmail.com>
2021-11-03 13:36:10 +0100

    Modifcation structure

--- app.js
+++ app.js
@@ -4,15 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000 ;
 
-// add router
-var indexRouter = require('./routes/index');
-
 app.get('/', function (req, res, next) {
-  console.log(req);
-  res.json('Bienvenue sur petits emprunts bientôt en react !');
+  res.json('Bienvenue dans notre API');
 });
 
-app.use("/", indexRouter);
 app.use("/mortalite",require('./routes/mortalite'))
 
 app.listen(PORT, function () {
