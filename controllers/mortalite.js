aa819e7b593e652aafa368c6f6da7d2c47572e1e
NFL <lulu57580@gmail.com>
2021-11-03 13:46:53 +0100

    Mise à niveau

--- controllers/mortalite.js
+++ controllers/mortalite.js
@@ -1,10 +1,11 @@
-//var functions = require("../public/javascript/fonction_share");
-
 exports.number_vac = (res, req) => {
+    //console.log(req.params.ids);
+    /*
     var num_dep = req.params.id;
     var data_set_1 = functions.get_dep_info(req.params.id);
     var data_set_2 = functions.get_dep_info(req.params.id);
     // Reception de records
+    */
     var json = { 
         "name": "Sara",
         "age": 23,
@@ -15,3 +16,4 @@ exports.number_vac = (res, req) => {
 
     console.log(json.age);
 }
+
--- routes/mortalite.js
+++ routes/mortalite.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const controller=require("../controllers/mortalite");
 
-router.get('/:id', controller.get_taux);
+router.get('/:id', controller.number_vac);
 
 module.exports=router;
