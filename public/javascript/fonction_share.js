8ebb3681f09b307c813d6fd1bdece0e8d23f73da
NFL <lulu57580@gmail.com>
2021-11-03 14:11:35 +0100

    Mise à niveau

--- public/javascript/fonction_share.js
+++ public/javascript/fonction_share.js
@@ -1,3 +1,5 @@
+const help = require(".")
+
 var urls = {
     vac : "https://public.opendatasoft.com/explore/dataset/covid-19-france-vaccinations-age-sexe-dep/table/?disjunctive.variable_label&sort=date&q=dep_code%3D34&dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJsaW5lIiwiZnVuYyI6IkFWRyIsInlBeGlzIjoibl9kb3NlMSIsInNjaWVudGlmaWNEaXNwbGF5Ijp0cnVlLCJjb2xvciI6IiNGRjUxNUEifV0sInhBeGlzIjoiZGF0ZSIsIm1heHBvaW50cyI6IiIsInRpbWVzY2FsZSI6InllYXIiLCJzb3J0IjoiIiwiY29uZmlnIjp7ImRhdGFzZXQiOiJjb3ZpZC0xOS1mcmFuY2UtdmFjY2luYXRpb25zLWFnZS1zZXhlLWRlcCIsIm9wdGlvbnMiOnsiZGlzanVuY3RpdmUudmFyaWFibGVfbGFiZWwiOnRydWUsInNvcnQiOiJkYXRlIiwicSI6ImRlcF9jb2RlPTM0In19fV0sImRpc3BsYXlMZWdlbmQiOnRydWUsImFsaWduTW9udGgiOnRydWUsInRpbWVzY2FsZSI6IiJ9",
     hos : "https://public.opendatasoft.com/api/records/1.0/search/?dataset=donnees-hospitalieres-covid-19-dep-france&q=dep_code%3D88&facet=date&facet=countrycode_iso_3166_1_alpha3&facet=region_min&facet=nom_dep_min&facet=sex"
@@ -14,6 +16,7 @@ exports.parse_to= function(type, data) {
     /**
      * Permet de renvoyer le format souhaité passé en paramètre
      */
+    
 }
 
 exports.get_file_type_requested = function (header_object) {
