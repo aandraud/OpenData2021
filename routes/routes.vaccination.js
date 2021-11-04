a2064385c48c9d3a3c0d320a7c51c91ef363caa2
NFL <lulu57580@gmail.com>
2021-11-04 09:27:53 +0100

    Ajout des routes

--- routes/routes.vaccination.js
+++ routes/routes.vaccination.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const controller=require("../controllers/controllers.vaccination");
 
-router.get('/nombre_vacine/', controller.get_number_recall);
+router.get('/nombre_vac/', controller.get_number_recall);
+
+router.get('/test/', controller.test_promise_1);
 
 router.get('',(req, res)=>{
     res.setHeader('Content-type','text/html')
-    res.status(200).send("<p>Bonjour, bienvenue dans la section vaccination. Vous pouvez effectuer de nombreuses requêtes, ces dernières sont décrites ici</p> \n <table><thead><tr><th colspan=''>Requêtes disponibles</th></tr></thead><tbody><tr><td>Récupérer les nombres de vacination</td><td>with two columns</td></tr></tbody></table>");
+    res.status(200).send("<p>Bonjour, bienvenue dans la section vaccination. Vous pouvez effectuer de nombreuses requêtes, ces dernières sont décrites ici</p> \n <table><thead><tr><th colspan=''>Requêtes disponibles</th></tr></thead><tbody><tr><td>Récupérer les nombres de vacination</td><td>URL</td></tr></tbody></table>");
 })
 
 module.exports=router;
