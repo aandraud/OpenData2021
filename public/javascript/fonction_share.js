0ccb619b8b93ac5d4e7e5fe1838fb64ee0d2e639
NFL <lulu57580@gmail.com>
2021-11-04 09:29:41 +0100

    Ajout des fonction share, exemple du fonctionnement des fonctions asynchrones

--- public/javascript/fonction_share.js
+++ public/javascript/fonction_share.js
@@ -1,85 +1,35 @@
 const { rejects } = require("assert");
 let http = require("http"),https = require("https");
 const { resolve } = require("path");
+let xmlJ=require('xml-js');
 
 
-exports.get_dep_info = function(id_dep) {
-    /**
-     * Permet de récupérer l'ensemble des information pour un département
-     */
-    console.log("Je suis dans get région",id_dep);
-
+exports.get_json = async function(id_dep){
     let url = 'https://public.opendatasoft.com/api/records/1.0/search/?dataset=covid-19-france-vaccinations-age-sexe-dep&q=&sort=date&rows=1000&refine.dep_code='+id_dep;
-
-    /*
-    let options = {json: true};
-
-    let response = request(url, options, (error, res, body) =>{
-        if(error){
-            resolve(error)
-        } else {
-            console.log(body);
-            resolve(body);
-        }
-    })
-
-    console.log(response);
-    */
-    
-    /*
-    let req = https.get(url, function(res){
-        let data = ''; 
-        let json_data;
-        res.on('data', function(stream){
-            data += stream
-        });
-        res.on('end', function(){
-            resolve(JSON.parse(data))
-            json_data = JSON.parse(data)
-            datas = json_data
-        })
-    })
-
-    req.on('error', function(e){
-        console.log(e.message);
-    })
-    */
-    
-   /*   
-    getJSON(url, function(error, response){
-    console.log(response);
-})
-*/
-
-
-    /*
     console.log(url);
-
-    https.get(url, function(res){
-        var body = '';
-        res.on('data', function(chunk){
-            body = +chunk;
+    return new Promise((resolve)=>{
+        https.get(url, res =>{
+            let data = "";
+            res.on('data', chunk => { data += chunk });
+            res.on('end',()=>{
+                json_data = JSON.parse(data);
+                resolve(json_data["records"]);
+            })
         })
-        res.on('end', function(){
-            console.log(body);
-            var Response = JSON.parse(body);
-            console.log("Got a response: ", Response);
-        });
-    }).on('error', function(e){
-        console.log(e.message);
     })
-    */
+}
 
-
-};
-
-exports.parse_to= function(type, data) {
-    return type, data;
+exports.parse_to= function(data) {
+    return new Promise(resolve => {
+        var options = {compact: true, ignoreComment: true, spaces: 4};
+        var result = xmlJ.json2xml(data, options);
+        resolve(result);
+    })
     /**
      * Permet de renvoyer le format souhaité passé en paramètre
      */
-    
 }
+
 
 exports.get_file_type_requested = function (header_object) {
     /**
