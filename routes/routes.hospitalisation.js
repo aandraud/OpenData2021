83b10f9b75ae3547ca7a0da4fe44e98ab81077de
Amelia ANDRAUD <andraud.amelia@hotmail.com>
2021-11-05 09:34:20 +0100

    modifications des routes finales

--- routes/routes.hospitalisation.js
+++ routes/routes.hospitalisation.js
@@ -11,7 +11,7 @@ router.get('/test/', controller.test_promise_2);
 
 router.get('',(req, res)=>{
     res.setHeader('Content-type','text/html')
-    res.status(200).send("<p>Bonjour, bienvenue dans la section vaccination. Vous pouvez effectuer de nombreuses requêtes, ces dernières sont décrites ici</p> \n <table><thead><tr><th colspan=''>Requêtes disponibles</th></tr></thead><tbody><tr><td>Récupérer les nombres de vacination</td><td>URL</td></tr></tbody></table>");
+    res.status(200).send("<p>Bonjour, bienvenue dans la section hospitalisation.</p> \n <table><thead><tr><th colspan=''>Requête disponible :</th></tr></thead><tbody><tr><td>Récupérer le nombre de patients en soins intensifs (day_intcare) parmi l'ensemble des patients hospitalisés (day_hosp) dans le département sélectionné au jour J-2</td></tr><tr><th colspan=''>URL :</th></tr></tbody></table>");
 })
 
 module.exports=router;
