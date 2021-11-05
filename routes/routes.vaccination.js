9c38a948f1ef8b3a82b5b4cee8b32abfd34fac1e
NFL <lulu57580@gmail.com>
2021-11-05 10:25:58 +0100

    Mise à niveau des routes

--- routes/routes.vaccination.js
+++ routes/routes.vaccination.js
@@ -5,6 +5,8 @@ const controller=require("../controllers/controllers.vaccination");
 
 router.get('/vaccination_par_dep',controller.get_number_vaccination_by_dep)
 
+router.get('/vaccination_national',controller.get_number_vaccination_national)
+
 router.get('/',(req, res)=>{
     res.setHeader('Content-type','text/html')
     res.status(200).send("<p>Bonjour, bienvenue dans la section vaccination. Vous pouvez effectuer de nombreuses requêtes, ces dernières sont décrites ici</p> \n <table><thead><tr><th colspan=''>Requêtes disponibles</th></tr></thead><tbody><tr><td>Récupérer les nombres de vacination</td><td>URL</td></tr></tbody></table>");
