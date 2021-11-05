b9e899feb0655249731787c3d53228402cc6c74c
Melody Rous <melodylise.rous@gmail.com>
2021-11-05 09:40:44 +0100

    modifs converData

--- public/javascript/convert.js
+++ public/javascript/convert.js
@@ -3,24 +3,22 @@ var js2xmlparser = require("js2xmlparser");
 function convertData(data, headersTypeAcepted){
 
     const xmlRegex = new RegExp('\/xml')
-    const xmlrdfRegex = new RegExp('\/xmlrdf')
-    let newData
-    let contentType = "application/json"
-    newData = JSON.parse(data)
+    const xmlrdfRegex = new RegExp('rdf')
+    
+    let contentType = "application/json" //default content type
+    //newData = JSON.parse(data)
 
-    if (xmlRegex.test(headersTypeAcepted)){
-      // set header
-      newData = js2xmlparser.parse("result", newData)
-      contentType = "application/xml"
-      console.dirxml(newData)
-    }
     if (xmlrdfRegex.test(headersTypeAcepted)){
-        // set header
-        newData = rdflib.parse("result", newData)
-        contentType = "application/rdfxml"
-        console.dirxml(newData)
+      data = rdflib.parse("result", data)
+      contentType = "application/rdf+xml"
+      //console.dirxml(data)
     }
-    return {"data":newData, "content_type":contentType}
+    else if (xmlRegex.test(headersTypeAcepted)){
+      data = js2xmlparser.parse("result", data)
+      contentType = "application/xml"
+      //console.dirxml(data)
+    }
+    return {"data":data, "content_type":contentType}
 };
 
 
