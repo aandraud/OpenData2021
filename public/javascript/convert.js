600afc6b7a819d921d8cdbe6f20e6493f49fb9af
Melody Rous <melodylise.rous@gmail.com>
2021-11-05 11:04:50 +0100

    finalisation de la fonction convertData

--- public/javascript/convert.js
+++ public/javascript/convert.js
@@ -1,6 +1,13 @@
 var js2xmlparser = require("js2xmlparser");
 
-function convertData(data, headersTypeAcepted){
+function convertData(data, headersTypeAcepted, reqType){
+  // Convert data to specific format requested. Prefered format is rdf+xml. Default format is Json. 
+  // Parameters:
+  // data, json object : processed data
+  // headersTypeAcceped, String : request.hearders.accept 
+  // reqType, String: id of the request type. Values accepted are "vac", "hosp", "both"
+  // Returns:
+  // Dictionary with data and content-type informations
 
     const xmlRegex = new RegExp('\/xml')
     const xmlrdfRegex = new RegExp('rdf')
@@ -9,7 +16,7 @@ function convertData(data, headersTypeAcepted){
     //newData = JSON.parse(data)
 
     if (xmlrdfRegex.test(headersTypeAcepted)){
-      data = rdflib.parse("result", data)
+      data = to_RDF(data, reqType)
       contentType = "application/rdf+xml"
       //console.dirxml(data)
     }
@@ -21,6 +28,12 @@ function convertData(data, headersTypeAcepted){
     return {"data":data, "content_type":contentType}
 };
 
+function to_RDF(data, reqType){
+  if (reqType == "vac") { return vaccination_to_RDF(data)}
+  else if (reqType == "hosp") { return hospitalisation_to_RDF(data)}
+  else if (reqType == "both") {return hosp_vacc_to_RDF(data)}
+  else {console.log("Error: Please select 'vac' 'hosp' or 'both' value for reqType parameter")}
+}
 
 function rdf (body) {
  return `<rdf:RDF 
@@ -31,22 +44,40 @@ function rdf (body) {
     </rdf>`
 }
 
-exports.vaccination_to_RDF = function (data){
-  body = `<igeo:Departement igeo:codeDepartement="${data["dep_code"]}" igeo:nom="${data["nom_dep_min"]}">
+vaccination_to_RDF = function (resp){
+  body = `<igeo:Departement igeo:codeDepartement="${resp["dep_code"]}" igeo:nom="${resp["dep_name"]}">
   <covidstats:has_statistique>
-      <covidstats:Count dcat:keyword="vaccination" covidstats:dateEffet="${data["date"]}" covidstats:has_count="${data["num_complet"]}" />
-      <covidstats:Taux dcat:keyword="vaccination" covidstats:dateEffet="${data["date"]}" covidstats:has_rate="${data["couv_complet"]}" />
+      <covidstats:Count covidstats:keyword="vaccination complete" covidstats:dateEffet="${resp["data"]["date"]}" covidstats:has_count="${resp["data"]["n_cum_complet"]}" />
+      <covidstats:Taux covidstats:keyword="vaccination complete" covidstats:dateEffet="${resp["data"]["date"]}" covidstats:has_rate="${resp["data"]["couv_complet"]}" />
   </covidstats:has_statistique>
   </igeo:Departement>`
   return rdf(body)
 }
 
-exports.hospitalisation_to_RDF = function(data){
-  body = `<igeo:Departement igeo:codeDepartement="${data["dep_code"]}" igeo:nom="${data["nom_dep_min"]}">
+hospitalisation_to_RDF = function(resp){
+  body = `<igeo:Departement igeo:codeDepartement="${resp["dep_code"]}" igeo:nom="${resp["dep_name"]}">
   <covidstats:has_statistique>
-      <covidstats:Count dcat:keyword="hospitalisation" covidstats:dateEffet="${data["date"]}" covidstats:has_count="${data["day_hosp"]}" />
-      <covidstats:Count dcat:keyword="soin intensif" covidstats:dateEffet="${data["date"]}" covidstats:has_count="${data["day_intcare"]}" />
+      <covidstats:Count covidstats:keyword="hospitalisation" covidstats:dateEffet="${resp["data"]["date"]}" covidstats:has_count="${resp["data"]["day_hosp"]}" />
+      <covidstats:Count covidstats:keyword="soin intensif" covidstats:dateEffet="${resp["data"]["date"]}" covidstats:has_count="${resp["data"]["day_intcare"]}" />
   </covidstats:has_statistique>
+</igeo:Departement>`
+return rdf(body)
+}
+
+hosp_vacc_to_RDF = function(resp){
+body = `  <igeo:Departement igeo:codeDepartement="${resp["dep_code"]}" igeo:nom="${resp["dep_name"]}">
+<covidstats:has_statistique>
+       <covidstats:from_url="vaccination"  >
+            <covidstats:Count covidstats:keyword="vaccination complete" covidstats:dateEffet="${resp["date"]["date"]}" covidstats:has_count="${resp["data"]["vaccination"]["n_cum_complet"]}" />
+            <covidstats:Taux covidstats:keyword="vaccination complete" covidstats:dateEffet="${resp["date"]}" covidstats:has_taux="${resp["data"]["vaccination"]["couv_complet"]}" />
+        </covidstats:from_url>
+        <covidstats:from_url="hospitalisation">
+            <covidstats:Count covidstats:keyword="hospitalisation" covidstats:dateEffet="${resp["data"]["date"]}" covidstats:has_count="${resp["data"]["hospitalisation"]["day_hosp"]}" />
+            <covidstats:Count covidstats:keyword="soin intensif" covidstats:dateEffet="${resp["data"]["date"]}" covidstats:has_count="${resp["data"]["hospitalisation"]["day_intcare"]}" />
+            <covidstats:Count covidstats:keyword="total deces" covidstats:dateEffet="${resp["data"]["date"]}" covidstats:has_count="${resp["data"]["hospitalisation"]["tot_death"]}" />
+            <covidstats:Count covidstats:keyword="total sorties" covidstats:dateEffet="${resp["data"]["date"]}" covidstats:has_count="${resp["data"]["hospitalisation"]["tot_out"]}" />
+        </covidstats:from_url>
+</covidstats:has_statistique>
 </igeo:Departement>`
 return rdf(body)
 }
