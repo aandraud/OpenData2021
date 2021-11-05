5e984d6b9742066359f5fb67fcd669bc20ca02c4
Melody Rous <melodylise.rous@gmail.com>
2021-11-05 14:02:12 +0100

    nettoyage code

--- app.js
+++ app.js
@@ -4,22 +4,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000 ;
 
-
 // add router
-
-
 app.use('/vaccination', require('./routes/routes.vaccination'))
-
 app.use('/hospitalisation', require('./routes/routes.hospitalisation'))
-
 app.use('/dep', require('./routes/routes.dep'))
 
-
-
-// add region
-var regionRouter = require('./routes/regions');
-app.use("/regions", regionRouter)
-
 app.listen(PORT, function () {
-  console.log('Petits emprunts lancé sur le port :' + PORT);
+  console.log('API OpenData2021 démarrée sur le port:' + PORT);
 });
