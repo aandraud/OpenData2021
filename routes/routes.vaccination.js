e962ef2f64d4862b1294b70dfd0bc249a069c0db
MinouDu34 <47379678+MinouDu34@users.noreply.github.com>
2021-11-05 13:36:49 +0100

    Update routes.vaccination.js

--- routes/routes.vaccination.js
+++ routes/routes.vaccination.js
@@ -9,7 +9,7 @@ router.get('/vaccination_national',controller.get_number_vaccination_national)
 
 router.get('/',(req, res)=>{
     res.setHeader('Content-type','text/html')
-    res.status(200).send("<p>Bonjour, bienvenue dans la section vaccination.</p> \n <table><thead><tr><th colspan=''>Requête disponible :</th></tr></thead><tbody><tr><td>Données à jour du schéma vaccinal complet (2 doses) selon deux variables sélectionnées (sexe et âge)</td></tr><tr><th colspan=''>URL : </th></tr><td><a>https://opendata2021.herokuapp.com/vaccination/vaccination_par_dep/?dep=34&variable=age</a></td></tbody></table>");
+    res.status(200).send("<p>Bonjour, bienvenue dans la section vaccination.</p> \n <table><thead><tr><th colspan=''>Requête disponible :</th></tr></thead><tbody><tr><td>Données à jour du schéma vaccinal complet (2 doses) selon deux variables sélectionnées (sexe et âge)</td></tr><tr><th colspan=''>URL : </th></tr><td><a href= "https://opendata2021.herokuapp.com/vaccination/vaccination_par_dep/?dep=34&variable=age">https://opendata2021.herokuapp.com/vaccination/vaccination_par_dep/?dep=34&variable=age</a></td></tbody></table>");
 })
 
 module.exports=router;
