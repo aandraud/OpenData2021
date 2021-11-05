1152d7e3e312924fb689af833882226b9f058896
Amelia ANDRAUD <andraud.amelia@hotmail.com>
2021-11-05 11:54:09 +0100

    modif html

--- routes/routes.hospitalisation.js
+++ routes/routes.hospitalisation.js
@@ -10,7 +10,7 @@ router.get('/hospitalisation_par_dep',controller.get_number_hospitalisation_by_d
 
 router.get('',(req, res)=>{
     res.setHeader('Content-type','text/html')
-    res.status(200).send("<p>Bonjour, bienvenue dans la section hospitalisation.</p> \n <table><thead><tr><th colspan=''>Requête disponible :</th></tr></thead><tbody><tr><td>Récupérer le nombre de patients en soins intensifs (day_intcare) parmi l'ensemble des patients hospitalisés (day_hosp) dans le département sélectionné au jour J-2</td></tr><tr><th colspan=''>URL :</th></tr></tbody></table>");
+    res.status(200).send("<p>Bonjour, bienvenue dans la section hospitalisation.</p> \n <table><thead><tr><th colspan=''>Requête disponible :</th></tr></thead><tbody><tr><td>Récupérer le nombre de patients en soins intensifs (day_intcare) parmi l'ensemble des patients hospitalisés (day_hosp) dans le département sélectionné au jour J-2</td></tr><tr><th colspan=''>URL : </th></tr><td><a>https://opendata2021.herokuapp.com/hospitalisation/hospitalisation_par_dep</a></td></tbody></table>");
 })
 
 module.exports=router;
