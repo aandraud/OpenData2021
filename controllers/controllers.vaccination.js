6cf9e3e3ea2f3fb7847e7a7d58c29ffa4f71303c
NFL <lulu57580@gmail.com>
2021-11-06 09:30:04 +0100

    Mise à jour

--- controllers/controllers.vaccination.js
+++ controllers/controllers.vaccination.js
@@ -1,7 +1,8 @@
-const functions = require('../public/javascript/fonction_share')
+const functions = require('../public/javascript/fonction_share');
+
 exports.get_number_vaccination_national = async function(req,res){
-    console.log(req.query);
     let result = await functions.get_from_opendata('','vac');
+    result = json_head_creation(result.data)
     res.status(200).send(result);
 }
 
@@ -17,7 +18,7 @@ exports.get_number_vaccination_by_dep = async function(req, res){
         if(Object.keys(req.query).length === 0){
             // Si la variable est vide > renvoie de l'ensemble des informations.
             result = await functions.get_from_opendata('variable_label%3D+et+Femmes','vac');
-            return res.status(200).json(json_head_creation(result.data));
+            return res.status(200).send(json_head_creation(result.data));
         } else {
             // Si la requête est supérieure à 0
             let request = "(dep_code%3D"+req.query['dep']+"+%26+variable_label%3D+et+Femmes)";
@@ -25,8 +26,8 @@ exports.get_number_vaccination_by_dep = async function(req, res){
             result = json_head_creation(result.data);
         }
         let parse = await functions.parse_to(result,'xml')
-        res.setHeader('Content-Type', 'text/'+'json');
-        res.status(200).send(result)
+        res.setHeader('Content-Type', 'text/'+'xml');
+        res.status(200).send(parse)
     } catch {
         res.status(400).json("Oups, une erreur est arrivée")
     }
