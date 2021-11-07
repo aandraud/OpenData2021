845ba94faa764a7b0b8f52c42f7ff350a435e174
Sina <nicolas.gauthiergngmail.com@Sinas-MacBook-Pro.local>
2021-11-07 10:30:47 +0100

    Ajout du path vers le vocab rdf dans appjs

--- app.js
+++ app.js
@@ -3,11 +3,24 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000 ;
+const fs = require('fs');
+//const file = require('./public/rdf/rdf_vocab.xml')
 
 // add router
 app.use('/vaccination', require('./routes/routes.vaccination'))
 app.use('/hospitalisation', require('./routes/routes.hospitalisation'))
 app.use('/dep', require('./routes/routes.dep'))
+
+
+app.get('/rdf_vocab',(req,res) => {
+  fs.readFile(__dirname + '/public/rdf/rdf_vocab.xml', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    res.send("<textarea style='border: none; margin: 0px; width: 894px; height: 870px;'>"+ data + "</textarea>")
+  });
+
+})
 
 app.listen(PORT, function () {
   console.log('API OpenData2021 démarrée sur le port:' + PORT);
